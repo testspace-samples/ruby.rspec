b324659787337cf476e23f8fc8333e7faa7e0160
Mark Underseth <marku@s2technologies.com>
2016-09-27 22:59:23 +0000

    setting env. var. when calling client

--- .c9build.sh
+++ .c9build.sh
@@ -10,9 +10,8 @@ bundle exec brakeman -o tmp/brakeman.json
 bundle exec brakeman_translate_checkstyle_format translate --file="tmp/brakeman.json" > tmp/brakeman_checkstyle.xml
 
 # Test
-export CI_REPORTS=$PWD/spec/reports
 COVERAGE=true xvfb-run --server-args="-screen 0 1024x768x24" bundle exec rake ci:setup:rspec spec
 
 # Publish
 curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
-testspace @.testspace master.c9
+CI_REPORTS=$PWD/spec/reports testspace @.testspace master.c9
