70fc6fbb5d3e4a17a8152cb3868eee95b06c0cec
Mark Underseth <marku@s2technologies.com>
2016-12-17 18:38:38 +0000

    add client download

--- .c9build.sh
+++ .c9build.sh
@@ -20,4 +20,5 @@ BRANCH_NAME=`git symbolic-ref --short HEAD`
 GIT_URL=`git remote show origin -n | grep Fetch\ URL: | sed 's/.*URL: //'`
 REPO_SLUG=`echo ${GIT_URL#*github.com?} | sed 's/.git//'`
 
+curl -s https://testspace-client.s3.amazonaws.com/testspace-linux.tgz | sudo tar -zxvf- -C /usr/local/bin
 CI_REPORTS=$PWD/spec/reports testspace @.testspace.txt $TESTSPACE_TOKEN/${REPO_SLUG/\//:}/${BRANCH_NAME}#c9.Build
